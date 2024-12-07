9d0395c9d424af6cc875fca47c85dc5731a7b5b0
Ismael Jiménez <87533490+rcnlvr@users.noreply.github.com>
2024-12-06 18:43:23 -0600

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -734,7 +734,7 @@ with tab6:
     #'Asset1': np.random.normal(0.01, 0.02, 100),
     #'Asset2': np.random.normal(0.02, 0.03, 100),
     #'Asset3': np.random.normal(0.015, 0.025, 100)
-})
+#})
 
 P = np.array([[1, -1, 0], [0, 1, -1]])
 Q = np.array([0.01, 0.02])
