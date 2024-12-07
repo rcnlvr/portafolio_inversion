5495b4ec1275a6cc819ca84ba5422d5ee6d4b8f3
Ismael Jiménez <87533490+rcnlvr@users.noreply.github.com>
2024-12-06 20:45:12 -0600

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -692,11 +692,7 @@ with tab5:
 with tab6:
     st.title('Cálculo de Riesgo con el Modelo de Black-Litterman')
     # Datos de ejemplo
-    returns = pd.DataFrame({
-    'Asset1': np.random.normal(0.01, 0.02, 100),
-    'Asset2': np.random.normal(0.02, 0.03, 100),
-    'Asset3': np.random.normal(0.015, 0.025, 100)
-    })
+    returns = pd.DataFrame(portfolio_returns)
 
     P = np.array([[1, -1, 0], [0, 1, -1]])
     Q = np.array([0.01, 0.02])
